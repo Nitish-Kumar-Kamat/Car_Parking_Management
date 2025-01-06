60978021181baef9112642fe319113ed73ab4604
Nitish <nitishkkamat08@gmail.com>
2025-01-06 16:23:29 +0530

    modified admin

--- car_parking_system/parking/admin.py
+++ car_parking_system/parking/admin.py
@@ -3,4 +3,4 @@ from .models import Entry_Vehicle
 
 @admin.register(Entry_Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('plate_number', 'gate_no', 'entry_time', 'level')
+    list_display = ('plate_number', 'gate_no', 'entry_time', 'level','slot')
